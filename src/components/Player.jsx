49fb9a1b10623709366b0fb6eb3db9e13217e44e
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-24 19:06:25 +0200

    TimerChallenge component is created and reused 4 times with different configuration i.e. different title and targetTime

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,12 +9,14 @@ export default function Player() {
   }
 
   return (
-    <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
-      <p>
-        <input ref={playerName} type="text" />
-        <button onClick={handleClick}>Set Name</button>
-      </p>
-    </section>
+    
+      <section id="player">
+        <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+        <p>
+          <input ref={playerName} type="text" />
+          <button onClick={handleClick}>Set Name</button>
+        </p>
+      </section>
+    
   );
 }
